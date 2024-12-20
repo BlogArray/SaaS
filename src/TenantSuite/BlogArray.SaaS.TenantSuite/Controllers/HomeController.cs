2d9fea7681a850b1b5a5382c442709188085b327
Sisindry Medagam <24821639+sisindrymedagam@users.noreply.github.com>
2024-12-20 22:11:44 +0530

    home

---
+++ src/TenantSuite/BlogArray.SaaS.TenantSuite/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BlogArray.SaaS.TenantSuite.Controllers;
+
+[Authorize]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return RedirectToActionPermanent("Index", "Dashboard");
+    }
+}
