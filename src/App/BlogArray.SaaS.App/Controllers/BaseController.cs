5366ad337556d3080a32f74233e23171c3ee66fe
Sisindry Medagam <24821639+sisindrymedagam@users.noreply.github.com>
2024-12-24 13:06:20 +0530

    personnels

---
+++ src/App/BlogArray.SaaS.App/Controllers/BaseController.cs
@@ -0,0 +1,69 @@
+﻿using BlogArray.SaaS.Mvc.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Net;
+
+namespace BlogArray.SaaS.App.Controllers;
+
+public class BaseController : Controller
+{
+    protected IActionResult JsonError(dynamic message)
+    {
+        ReturnResult returnResult = new()
+        {
+            Status = false,
+            Message = Converter.ToString(message)
+        };
+        return BadRequest(returnResult);
+    }
+
+    protected IActionResult ModelStateError(ModelStateDictionary ModelState)
+    {
+        IEnumerable<string> errors = ModelState.Values.SelectMany(state => state.Errors).Select(error => error.ErrorMessage);
+        ReturnResult returnResult = new()
+        {
+            Status = false,
+            Message = string.Join(". ", errors)
+        };
+        return BadRequest(returnResult);
+    }
+
+    protected IActionResult JsonSuccess(dynamic message)
+    {
+        ReturnResult returnResult = new()
+        {
+            Status = true,
+            Message = Converter.ToString(message)
+        };
+
+        return Ok(returnResult);
+    }
+
+    protected IActionResult Exception(HttpStatusCode statusCode)
+    {
+        throw new Exception(statusCode.ToString());
+    }
+
+    protected IActionResult IdentityErrorResult(IEnumerable<IdentityError> identityErrors)
+    {
+        string errors = string.Join(". ", identityErrors.Select(e => e.Description));
+
+        return JsonError(errors);
+    }
+
+    protected void AddErrorMessage(string error)
+    {
+        AddMessage("danger", error);
+    }
+
+    protected void AddSuccessMessage(string error)
+    {
+        AddMessage("success", error);
+    }
+
+    private void AddMessage(string type, string error)
+    {
+        TempData["AlertMessage"] = error;
+        TempData["AlertType"] = type;
+    }
+}
--- src/App/BlogArray.SaaS.App/Controllers/PersonnelsController.cs
+++ src/App/BlogArray.SaaS.App/Controllers/PersonnelsController.cs
@@ -1,6 +1,7 @@
 ﻿using BlogArray.SaaS.App.Interfaces;
 using BlogArray.SaaS.App.Models;
 using BlogArray.SaaS.TenantStore.App;
+using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using Refit;
 
@@ -8,9 +9,9 @@ using Refit;
 namespace BlogArray.SaaS.App.Controllers;
 
 [Microsoft.AspNetCore.Authorization.Authorize]
-public class UsersController(SaasAppDbContext context,
+public class PersonnelsController(SaasAppDbContext context,
      IMultiTenantContextAccessor<AppTenantInfo> multiTenantContextAccessor,
-     IMembershipClient membershipClient, ILogger<UsersController> logger) : Controller
+     IMembershipClient membershipClient, ILogger<PersonnelsController> logger) : BaseController
 {
     public async Task<IActionResult> Index()
     {
@@ -42,13 +43,6 @@ public class UsersController(SaasAppDbContext context,
             return View(userVM);
         }
 
-        await context.AppPersonnels.AddAsync(new AppPersonnel
-        {
-            Email = userVM.Email,
-            IsActive = userVM.IsActive
-        });
-        await context.SaveChangesAsync();
-
         try
         {
             await membershipClient.Invite(new UserTenantVM
@@ -56,23 +50,29 @@ public class UsersController(SaasAppDbContext context,
                 Email = userVM.Email,
                 Tenant = multiTenantContextAccessor.MultiTenantContext.TenantInfo.Identifier
             });
+
+            await context.AppPersonnels.AddAsync(new AppPersonnel
+            {
+                Email = userVM.Email,
+                IsActive = userVM.IsActive
+            });
+            await context.SaveChangesAsync();
+            AddSuccessMessage($"Personnel {userVM.Email} is successfully added.");
         }
         catch (ApiException apiException)
         {
+            AddErrorMessage($"Unable to invite personnel with email {userVM.Email}.");
             logger.LogError("The API returned an exception with status code {0} with content {1}", apiException.StatusCode, apiException.Content);
         }
 
         return RedirectToAction("Index");
     }
 
-
     public async Task<IActionResult> DeActivate(long id)
     {
         var user = await context.AppPersonnels.SingleOrDefaultAsync(u => u.Id == id);
         if (user is not null)
         {
-            user.IsActive = false;
-            await context.SaveChangesAsync();
             try
             {
                 await membershipClient.RemoveUserFromTenant(new UserTenantVM
@@ -80,9 +80,14 @@ public class UsersController(SaasAppDbContext context,
                     Email = user.Email,
                     Tenant = multiTenantContextAccessor.MultiTenantContext.TenantInfo.Identifier
                 });
+                user.IsActive = false;
+                await context.SaveChangesAsync();
+
+                AddSuccessMessage($"Personnel {user.Email} is deactivated successfully.");
             }
             catch (ApiException apiException)
             {
+                AddErrorMessage($"Unable to deactivate personnel with email {user.Email}.");
                 logger.LogError("The API returned an exception with status code {0} with content {1}", apiException.StatusCode, apiException.Content);
             }
         }
@@ -94,9 +99,6 @@ public class UsersController(SaasAppDbContext context,
         var user = await context.AppPersonnels.SingleOrDefaultAsync(u => u.Id == id);
         if (user is not null)
         {
-            user.IsActive = true;
-            await context.SaveChangesAsync();
-
             try
             {
                 await membershipClient.AddUserToTenant(new UserTenantVM
@@ -104,9 +106,13 @@ public class UsersController(SaasAppDbContext context,
                     Email = user.Email,
                     Tenant = multiTenantContextAccessor.MultiTenantContext.TenantInfo.Identifier
                 });
+                user.IsActive = true;
+                await context.SaveChangesAsync();
+                AddSuccessMessage($"Personnel {user.Email} is activated successfully.");
             }
             catch (ApiException apiException)
             {
+                AddErrorMessage($"Unable to activate personnel with email {user.Email}.");
                 logger.LogError("The API returned an exception with status code {0} with content {1}", apiException.StatusCode, apiException.Content);
             }
         }
