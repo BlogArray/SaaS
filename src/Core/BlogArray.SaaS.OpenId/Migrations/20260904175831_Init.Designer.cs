84e4ee082a75925bee0cdbb63525b41fcbea8dcf
Sisindry Medagam <24821639+sisindrymedagam@users.noreply.github.com>
2026-09-04 23:31:32 +0530

    Removed old migrations and created single migration

--- src/Core/BlogArray.SaaS.OpenId/Migrations/20260904175831_Init.Designer.cs
+++ src/Core/BlogArray.SaaS.OpenId/Migrations/20260904175831_Init.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogArray.SaaS.OpenId.Migrations
 {
     [DbContext(typeof(OpenIdDbContext))]
-    [Migration("20260904102614_AddClientIdToSecurityEvent")]
-    partial class AddClientIdToSecurityEvent
+    [Migration("20260904175831_Init")]
+    partial class Init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -226,6 +226,78 @@ namespace BlogArray.SaaS.OpenId.Migrations
                             TwoFactorEnabled = false,
                             UserName = "admin@blogarray.net"
                         });
+                });
+
+            modelBuilder.Entity("BlogArray.SaaS.Domain.Entities.AuditEvent", b =>
+                {
+                    b.Property<string>("Id")
+                        .HasMaxLength(400)
+                        .HasColumnType("nvarchar(400)");
+
+                    b.Property<string>("ClientId")
+                        .HasMaxLength(400)
+                        .HasColumnType("nvarchar(400)");
+
+                    b.Property<DateTime>("CreatedOn")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("DeviceInfo")
+                        .HasMaxLength(256)
+                        .HasColumnType("nvarchar(256)");
+
+                    b.Property<string>("EventType")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
+
+                    b.Property<string>("IpAddress")
+                        .HasMaxLength(64)
+                        .HasColumnType("nvarchar(64)");
+
+                    b.Property<string>("NewValue")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("OldValue")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Reason")
+                        .HasMaxLength(512)
+                        .HasColumnType("nvarchar(512)");
+
+                    b.Property<string>("Result")
+                        .IsRequired()
+                        .HasMaxLength(20)
+                        .HasColumnType("nvarchar(20)");
+
+                    b.Property<string>("TargetUserId")
+                        .HasMaxLength(400)
+                        .HasColumnType("nvarchar(400)");
+
+                    b.Property<string>("TriggeredBy")
+                        .IsRequired()
+                        .HasMaxLength(20)
+                        .HasColumnType("nvarchar(20)");
+
+                    b.Property<string>("UserAgent")
+                        .HasMaxLength(512)
+                        .HasColumnType("nvarchar(512)");
+
+                    b.Property<string>("UserId")
+                        .IsRequired()
+                        .HasMaxLength(400)
+                        .HasColumnType("nvarchar(400)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("TargetUserId");
+
+                    b.HasIndex("ClientId", "CreatedOn");
+
+                    b.HasIndex("EventType", "CreatedOn");
+
+                    b.HasIndex("UserId", "CreatedOn");
+
+                    b.ToTable("AuditEvents");
                 });
 
             modelBuilder.Entity("BlogArray.SaaS.Domain.Entities.OpenIdApplication", b =>
@@ -540,11 +612,15 @@ namespace BlogArray.SaaS.OpenId.Migrations
                     b.ToTable("PasswordHistories");
                 });
 
-            modelBuilder.Entity("BlogArray.SaaS.Domain.Entities.SecurityEvent", b =>
+            modelBuilder.Entity("BlogArray.SaaS.Domain.Entities.SignInEvent", b =>
                 {
                     b.Property<string>("Id")
                         .HasMaxLength(400)
                         .HasColumnType("nvarchar(400)");
+
+                    b.Property<string>("AuthMethod")
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
 
                     b.Property<string>("ClientId")
                         .HasMaxLength(400)
@@ -557,6 +633,10 @@ namespace BlogArray.SaaS.OpenId.Migrations
                         .HasMaxLength(512)
                         .HasColumnType("nvarchar(512)");
 
+                    b.Property<string>("DeviceInfo")
+                        .HasMaxLength(256)
+                        .HasColumnType("nvarchar(256)");
+
                     b.Property<string>("EventType")
                         .IsRequired()
                         .HasMaxLength(100)
@@ -565,6 +645,11 @@ namespace BlogArray.SaaS.OpenId.Migrations
                     b.Property<string>("IpAddress")
                         .HasMaxLength(64)
                         .HasColumnType("nvarchar(64)");
+
+                    b.Property<string>("Result")
+                        .IsRequired()
+                        .HasMaxLength(20)
+                        .HasColumnType("nvarchar(20)");
 
                     b.Property<string>("UserAgent")
                         .HasMaxLength(512)
@@ -579,9 +664,11 @@ namespace BlogArray.SaaS.OpenId.Migrations
 
                     b.HasIndex("CreatedOn");
 
-                    b.HasIndex("UserId");
+                    b.HasIndex("ClientId", "CreatedOn");
 
-                    b.ToTable("SecurityEvents");
+                    b.HasIndex("UserId", "CreatedOn");
+
+                    b.ToTable("SignInEvents");
                 });
 
             modelBuilder.Entity("BlogArray.SaaS.Domain.Entities.UserSession", b =>
@@ -971,7 +1058,7 @@ namespace BlogArray.SaaS.OpenId.Migrations
                         .IsRequired();
                 });
 
-            modelBuilder.Entity("BlogArray.SaaS.Domain.Entities.SecurityEvent", b =>
+            modelBuilder.Entity("BlogArray.SaaS.Domain.Entities.SignInEvent", b =>
                 {
                     b.HasOne("BlogArray.SaaS.Domain.Entities.ApplicationUser", null)
                         .WithMany()
